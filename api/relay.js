ee1881b9393fd6996c093a931a1382c1f2e7b634
-https.128615727.my <xemzreal@gmail.com>
2025-07-13 12:32:10 +0700

    Update relay.js

--- api/relay.js
+++ api/relay.js
@@ -1,12 +1,12 @@
 ({
-  domainV2: "https://kenja-ganteng.kenjaapublik.my.id", // Panel Pterodactyl host
-  apikeyV2: "apixxx",                                   // Application API Key (server creation)
-  capikeyV2: "pltaxxx",                                 // Client API Key (jika diperlukan)
-  eggV2: "15",                                           // Egg ID (misal: nodejs, python, dll)
-  nestidV2: "5",                                         // Nest ID (kategori egg)
-  locV2: "1",                                            // Lokasi server (ID location Pterodactyl)
-  dockerImage: "ghcr.io/parkervcp/yolks:nodejs_18",     // Docker image default
-  environment: {                                        // Environment variables default
+  domainV2: "https://kenja-ganteng.kenjaapublik.my.id",
+  apikeyV2: "apixxx",
+  capikeyV2: "pltaxxx",
+  eggV2: "15",
+  nestidV2: "5",
+  locV2: "1",
+  dockerImage: "ghcr.io/parkervcp/yolks:nodejs_18",
+  environment: {
     INST: "npm",
     USER_UPLOAD: "0",
     AUTO_UPDATE: "0",
@@ -17,8 +17,8 @@
     backups: 5,
     allocations: 5
   },
-  io: 500, // IO priority default
-  node: 2, // Nomor node (opsional buat tampil di UI)
+  io: 500,
+  node: 2,
   info: {
     masaAktif: "30 Hari",
     garansi: "15 Hari (1x Replace)",
